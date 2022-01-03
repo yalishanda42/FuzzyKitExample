c7e6a17c1586ab1ae71075fefa2f72e6d3140fdc
Alexander Ignatov <yalishanda@abv.bg>
2022-01-04 01:16:58 +0200

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Alexander Ignatov on 4.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/FuzzyExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuzzyExampleApp.swift
+//  Shared
+//
+//  Created by Alexander Ignatov on 4.01.22.
+//
+
+import SwiftUI
+
+@main
+struct FuzzyExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
